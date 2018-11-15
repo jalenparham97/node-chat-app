71d22eec49258ed744c3efd05be232b6f44a9da0
jalen <jalenparham97@gmail.com>
2018-11-15 11:21:27 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+
+const public = path.join(__dirname, '../public')
+const app = express()
+
+app.use(express.static(public))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`)
+})
